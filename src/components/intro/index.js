a37db6c0c15ee6b7d6e48af1c67daa28369ed098
Szymon Kaliski <hi@szymonkaliski.com>
2017-07-19 18:47:25 +0800

    added example p5 codes

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import { addInputCode, showInfo } from '../../actions';
+import { EXAMPLES } from '../../constants';
 
 import './index.css';
 
@@ -34,15 +35,27 @@ class Intro extends Component {
       <div className="intro">
         <div className="intro__text">
           welcome to <span className="intro__logo">Parametrium</span> — interactive parameter space explorer for P5.js
-          <div className="intro__info-btn" onClick={this.props.showInfo}>info</div>
+          <div className="intro__info-btn" onClick={this.props.showInfo}>
+            info
+          </div>
         </div>
 
         <div className="intro__content">
           <Dropzone className="dropzone" activeClassName="dropzone--active" onDrop={this.onDrop} multiple={false}>
-            <span className="dropzone__text">
-              drop sketch here, or click to select
-            </span>
+            <span className="dropzone__text">drop sketch here, or click to select</span>
           </Dropzone>
+        </div>
+
+        <div className="intro__examples">
+          <div className="intro__examples-text">or load one of examples:</div>
+
+          <ul className="intro__examples-list">
+            {EXAMPLES.map(({ name, url, code }) =>
+              <li className="intro__examples-list-item" onClick={() => this.props.addInputCode(code)}>
+                {name} (<a className="intro__examples-original-link" href={url}>original</a>)
+              </li>
+            )}
+          </ul>
         </div>
       </div>
     );
--- src/constants.js
+++ src/constants.js
@@ -15,3 +15,346 @@ export const LITERAL_TYPES = [
   'COLOR', // from COLOR_CALLESS
   'CONTROL' // for & while
 ].reduce((acc, key) => ({ ...acc, [key]: key }), {});
+
+export const EXAMPLES = [
+  {
+    name: 'recursion',
+    url: 'https://p5js.org/examples/structure-recursion.html',
+    code: `
+function setup() {
+  createCanvas(720, 400);
+  noStroke();
+  noLoop();
+}
+
+function draw() {
+  drawCircle(width/2, 280, 6);
+}
+
+function drawCircle(x, radius, level) {
+  var tt = 126 * level/4.0;
+  fill(tt);
+  ellipse(x, height/2, radius*2, radius*2);
+  if(level > 1) {
+    level = level - 1;
+    drawCircle(x - radius/2, radius/2, level);
+    drawCircle(x + radius/2, radius/2, level);
+  }
+}`
+  },
+  {
+    name: 'primitives',
+    url: 'https://p5js.org/examples/form-shape-primitives.html',
+    code: `
+function setup() {
+// Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 400);
+  background(0);
+  noStroke();
+
+  fill(204);
+  triangle(18, 18, 18, 360, 81, 360);
+
+  fill(102);
+  rect(81, 81, 63, 63);
+
+  fill(204);
+  quad(189, 18, 216, 18, 216, 360, 144, 360);
+
+  fill(255);
+  ellipse(252, 144, 72, 72);
+
+  fill(204);
+  triangle(288, 18, 351, 360, 288, 360);
+
+  fill(255);
+  arc(479, 300, 280, 280, PI, TWO_PI);
+}`
+  },
+  {
+    name: 'particle system',
+    url: 'https://p5js.org/examples/simulate-particle-system.html',
+    code: `
+var system;
+
+function setup() {
+  createCanvas(720, 400);
+  system = new ParticleSystem(createVector(width/2, 50));
+}
+
+function draw() {
+  background(51);
+  system.addParticle();
+  system.run();
+}
+
+// A simple Particle class
+var Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255.0;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  if (this.lifespan < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (var i = this.particles.length-1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};`
+  },
+  {
+    name: 'spirograph',
+    url: 'https://p5js.org/examples/simulate-spirograph.html',
+    code: `
+var NUMSINES = 20; // how many of these things can we do at once?
+var sines = new Array(NUMSINES); // an array to hold all the current angles
+var rad; // an initial radius value for the central sine
+var i; // a counter variable
+
+// play with these to get a sense of what's going on:
+var fund = 0.005; // the speed of the central sine
+var ratio = 1; // what multiplier for speed is each additional sine?
+var alpha = 50; // how opaque is the tracing system
+
+var trace = false; // are we tracing?
+
+function setup() {
+  createCanvas(710, 400);
+
+  rad = height/4; // compute radius for central circle
+  background(204); // clear the screen
+
+  for (var i = 0; i<sines.length; i++) {
+    sines[i] = PI; // start EVERYBODY facing NORTH
+  }
+}
+
+function draw() {
+  if (!trace) {
+    background(204); // clear screen if showing geometry
+    stroke(0, 255); // black pen
+    noFill(); // don't fill
+  }
+
+  // MAIN ACTION
+  push(); // start a transformation matrix
+  translate(width/2, height/2); // move to middle of screen
+
+  for (var i = 0; i<sines.length; i++) {
+    var erad = 0; // radius for small "point" within circle... this is the 'pen' when tracing
+    // setup for tracing
+    if (trace) {
+      stroke(0, 0, 255*(float(i)/sines.length), alpha); // blue
+      fill(0, 0, 255, alpha/2); // also, um, blue
+      erad = 5.0*(1.0-float(i)/sines.length); // pen width will be related to which sine
+    }
+    var radius = rad/(i+1); // radius for circle itself
+    rotate(sines[i]); // rotate circle
+    if (!trace) ellipse(0, 0, radius*2, radius*2); // if we're simulating, draw the sine
+    push(); // go up one level
+    translate(0, radius); // move to sine edge
+    if (!trace) ellipse(0, 0, 5, 5); // draw a little circle
+    if (trace) ellipse(0, 0, erad, erad); // draw with erad if tracing
+    pop(); // go down one level
+    translate(0, radius); // move into position for next sine
+    sines[i] = (sines[i]+(fund+(fund*i*ratio)))%TWO_PI; // update angle based on fundamental
+  }
+
+  pop(); // pop down final transformation
+
+}
+
+function keyReleased() {
+  if (key==' ') {
+    trace = !trace;
+    background(255);
+  }
+}`
+  },
+  {
+    name: 'l-systems',
+    url: 'https://p5js.org/examples/simulate-l-systems.html',
+    code: `
+// TURTLE STUFF:
+var x, y; // the current position of the turtle
+var currentangle = 0; // which way the turtle is pointing
+var step = 20; // how much the turtle moves with each 'F'
+var angle = 90; // how much the turtle turns with a '-' or '+'
+
+// LINDENMAYER STUFF (L-SYSTEMS)
+var thestring = 'A'; // "axiom" or start of the string
+var numloops = 5; // how many iterations to pre-compute
+var therules = []; // array for rules
+therules[0] = ['A', '-BF+AFA+FB-']; // first rule
+therules[1] = ['B', '+AF-BFB-FA+']; // second rule
+
+var whereinstring = 0; // where in the L-system are we?
+
+function setup() {
+  createCanvas(710, 400);
+  background(255);
+  stroke(0, 0, 0, 255);
+
+  // start the x and y position at lower-left corner
+  x = 0;
+  y = height-1;
+
+  // COMPUTE THE L-SYSTEM
+  for (var i = 0; i < numloops; i++) {
+    thestring = lindenmayer(thestring);
+  }
+}
+
+function draw() {
+
+  // draw the current character in the string:
+  drawIt(thestring[whereinstring]);
+
+  // increment the point for where we're reading the string.
+  // wrap around at the end.
+  whereinstring++;
+  if (whereinstring > thestring.length-1) whereinstring = 0;
+
+}
+
+// interpret an L-system
+function lindenmayer(s) {
+  var outputstring = ''; // start a blank output string
+
+  // iterate through 'therules' looking for symbol matches:
+  for (var i = 0; i < s.length; i++) {
+    var ismatch = 0; // by default, no match
+    for (var j = 0; j < therules.length; j++) {
+      if (s[i] == therules[j][0])  {
+        outputstring += therules[j][1]; // write substitution
+        ismatch = 1; // we have a match, so don't copy over symbol
+        break; // get outta this for() loop
+      }
+    }
+    // if nothing matches, just copy the symbol over.
+    if (ismatch == 0) outputstring+= s[i];
+  }
+
+  return outputstring; // send out the modified string
+}
+
+// this is a custom function that draws turtle commands
+function drawIt(k) {
+
+  if (k=='F') { // draw forward
+    // polar to cartesian based on step and currentangle:
+    var x1 = x + step*cos(radians(currentangle));
+    var y1 = y + step*sin(radians(currentangle));
+    line(x, y, x1, y1); // connect the old and the new
+
+    // update the turtle's position:
+    x = x1;
+    y = y1;
+  } else if (k == '+') {
+    currentangle += angle; // turn left
+  } else if (k == '-') {
+    currentangle -= angle; // turn right
+  }
+
+  // give me some random color values:
+  var r = random(128, 255);
+  var g = random(0, 192);
+  var b = random(0, 50);
+  var a = random(50, 100);
+
+  // pick a gaussian (D&D) distribution for the radius:
+  var radius = 0;
+  radius += random(0, 15);
+  radius += random(0, 15);
+  radius += random(0, 15);
+  radius = radius/3;
+
+  // draw the stuff:
+  fill(r, g, b, a);
+  ellipse(x, y, radius, radius);
+}`
+  },
+  {
+    name: 'lerp color',
+    url: 'https://p5js.org/examples/color-lerp-color.html',
+    code: `
+function setup() {
+  createCanvas(720, 400);
+  background(255);
+  noStroke();
+}
+
+function draw() {
+  background(255);
+  from = color(255, 0, 0, 0.2 * 255);
+  to = color(0, 0, 255, 0.2 * 255);
+  c1 = lerpColor(from, to, .33);
+  c2 = lerpColor(from, to, .66);
+  for (var i = 0; i < 15; i++) {
+    fill(from);
+    quad(random(-40, 220), random(height),
+         random(-40, 220), random(height),
+         random(-40, 220), random(height),
+         random(-40, 220), random(height));
+    fill(c1);
+    quad(random(140, 380), random(height),
+         random(140, 380), random(height),
+         random(140, 380), random(height),
+         random(140, 380), random(height));
+    fill(c2);
+    quad(random(320, 580), random(height),
+         random(320, 580), random(height),
+         random(320, 580), random(height),
+         random(320, 580), random(height));
+    fill(to);
+    quad(random(500, 760), random(height),
+         random(500, 760), random(height),
+         random(500, 760), random(height),
+         random(500, 760), random(height));
+  }
+  frameRate(5);
+}`
+  }
+];
